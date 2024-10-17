7d13ff9e40755dea3596829b9ee6e3f8d53f85ec
Anton Chebanitsa <a.chebanitsa@godeltech.com>
2024-10-17 18:37:17 +0300

    add nunjucks and view for meteors

--- app/delivery/routes.js
+++ app/delivery/routes.js
@@ -1,21 +1,26 @@
 ﻿const express = require('express');
-const { getAsteroidData } = require('../usecases/getAsteroidData');
-const { nasaApiUrl, nasaApiKey } = require('../../config');
+const {getAsteroidData} = require('../usecases/getAsteroidData');
+const {nasaApiUrl, nasaApiKey} = require('../../config');
 const Exception = require("../customExceptions/Exception");
-const { calculateDateRange } = require('../utils/utils');
+const {calculateDateRange} = require('../utils/utils');
 
 const router = express.Router();
 
 router.get('/meteors', async (req, res, next) => {
     try {
-        const { start_date, end_date, sentry_objects_count, were_dangerous_meteors } = req.query;
-        const { startDate, endDate } = calculateDateRange(start_date, end_date);
+        const {start_date, end_date, sentry_objects_count, were_dangerous_meteors} = req.query;
+        const {startDate, endDate} = calculateDateRange(start_date, end_date);
 
         const checkSentryObjectsCount = sentry_objects_count?.trim() === 'true';
         const checkDangerousMeteors = were_dangerous_meteors?.trim() === 'true';
 
         const data = await getAsteroidData(nasaApiUrl, nasaApiKey, startDate, endDate, checkSentryObjectsCount, checkDangerousMeteors);
-        res.json(data);
+
+        res.render('meteors', {
+            data: data.data,
+            wereDangerousMeteors: data.wereDangerousMeteors,
+            sentryObjectsCount: data.sentryObjectsCount
+        });
     } catch (error) {
         next(new Exception(error.statusCode, error.message));
     }
--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const express = require('express');
 const routes = require('./app/delivery/routes');
 const errorHandler = require('./app/middleware/errorHandler');
 const { port } = require('./config');
+const nunjucks = require('nunjucks');
 
 const app = express();
+
+nunjucks.configure('app/views', {
+    autoescape: true,
+    express: app
+});
+
+app.set('view engine', 'njk');
 
 app.use('/', routes);
 app.use(errorHandler);
