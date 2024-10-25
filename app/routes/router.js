55636dac4c6ccfe269910eae14bf455b24508cb6
Anton Chebanitsa <a.chebanitsa@godeltech.com>
2024-10-25 10:49:40 +0200

    fix incorrect conflict resolution

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,10 +1,12 @@
 ﻿const express = require('express');
 const meteorsController = require('./controllers/meteor.controller');
-const roverController = require('./controllers/roverImage.controller');
+const roverImageController = require('./controllers/roverImage.controller');
+const validateRequest = require("../middleware/validationMiddleware");
+const {meteorsSchema, roverImageSchema} = require("../validation/schemas");
 
 const router = express.Router();
 
-router.use(meteorsController);
-router.use(roverController);
+router.post('/rover-image', validateRequest(roverImageSchema), roverImageController);
+router.get('/meteors', validateRequest(meteorsSchema), meteorsController);
 
 module.exports = router;
